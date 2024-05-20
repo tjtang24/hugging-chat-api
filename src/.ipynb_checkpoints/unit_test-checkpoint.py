864c37e89f2d21e4f7316b36667536c492ca3418
TJ Tang <89395985+tjtang24@users.noreply.github.com>
2024-05-20 13:30:01 -0400

    fix

---
+++ src/.ipynb_checkpoints/unit_test-checkpoint.py
@@ -0,0 +1,88 @@
+"""
+For test hugchat
+"""
+
+# import os
+import logging
+
+from .hugchat.message import MSGSTATUS_RESOLVED, RESPONSE_TYPE_FINAL, Message, MSGTYPE_ERROR, RESPONSE_TYPE_WEB
+import sys
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class MockResponse():
+    """
+    mock a respone from hugchat api
+    """
+
+    def __init__(self, response_list):
+        self.index = 0
+        self.response_list = response_list
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.response_list):
+            raise StopIteration
+        result = self.response_list[self.index]
+        self.index += 1
+        return result
+
+
+class Test(object):
+    """
+    test hugchat api
+    """
+
+    def test_web_search_failed_results(self):
+        response_list = [{
+            "type":
+            RESPONSE_TYPE_WEB,
+            "messageType":
+            MSGTYPE_ERROR,
+            "message":
+            "Failed to parse webpage",
+            "args": [
+                "This operation was aborted",
+                "https://www.accuweather.com/en/gb/london/ec4a-2/weather-forecast/328328"
+            ]
+        }, {
+            "type":
+            RESPONSE_TYPE_WEB,
+            "messageType":
+            RESPONSE_TYPE_WEB,
+            "sources": [{
+                "title": "1",
+                "link": "2",
+                "hostname": "3"
+            }]
+        }, {
+            "type":
+            RESPONSE_TYPE_WEB,
+            "messageType":
+            MSGTYPE_ERROR,
+            "message":
+            "Failed to parse webpage",
+            "args": [
+                "This operation was aborted",
+                "https://www.accuweather.com/en/gb/london/ec4a-2/weather-forecast/328328"
+            ]
+        }, {
+            "type": RESPONSE_TYPE_FINAL,
+            "messageType": "answer",
+            "text": "Funny joke"
+        }]
+
+        mock = MockResponse(response_list)
+        response = Message(mock, web_search=True)
+        while True:
+            try:
+                print(response)
+                response = next(response)
+            except StopIteration:
+                break
+        assert len(response.web_search_sources
+                   ) == 1  # only the second web search response is valid.
+        assert response.text == "Funny joke"
